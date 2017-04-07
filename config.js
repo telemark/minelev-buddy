f2388cfa4aa4f420fefbac81ac7d75d487982a3c
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-07 07:22:33 +0200

    Adds winston and papertrail

--- config.js
+++ config.js
@@ -2,5 +2,8 @@
 
 module.exports = {
   BUDDY_API: process.env.BUDDY_API || 'https://dummy.buddy.minelev.t-fk.win',
-  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go'
+  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
+  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-buddy',
+  PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
+  PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
 }
