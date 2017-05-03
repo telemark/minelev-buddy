287ce42e83c25bc9f088995a366bb28689cf3b74
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-03 11:46:10 +0200

    Changes demo settings

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  BUDDY_API: process.env.BUDDY_API || 'https://dummy.buddy.minelev.t-fk.win',
+  BUDDY_API: process.env.BUDDY_API || 'https://dummy.buddy.minelev.win',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-buddy',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
